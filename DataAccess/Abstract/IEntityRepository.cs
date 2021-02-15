f6e6c6abf63398689129891030c6086f5007bcdf
Süleyman <Suleymanpsp38@gmail.com>
2021-02-15 18:25:07 +0300

    MyFinalProjectExercise

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    // generic constraint
+    // class : referans tip
+    // IEntity : IEntity olabilir veya IEntity impelemente eden bir nesne olabilir
+    // new() : new'lenebilir olmalı
+    public interface IEntityRepository<T>
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+    }
+}
