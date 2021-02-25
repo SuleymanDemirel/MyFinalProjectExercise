873bdc3bfc32823402b0c6cf0dd7a87835cf56a8
Süleyman <Suleymanpsp38@gmail.com>
2021-02-25 20:07:39 +0300

    Web-Abi-Added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -34,7 +34,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-          
+            if (DateTime.Now.Hour == 13)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            }
             
            // data result dönecek,Çalıştığım tip,Datam,True,Bilgilendirici mesajım
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // Loosely coupled-Gevşek bağlılık(soyuta bağlılık)
+        // IoC Container -- Inversion of Control
+        //              Naming convention 
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll() 
+        {
+            
+            var result = _productService.GetAll();
+            if (result.Success)//default true ise
+            {
+                return Ok(result); // 200 
+            }
+            return BadRequest(result); // 400 
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id) 
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product) // client'dan,angular'dan,react'dan vs. gönderdiğin ürünü buraya koy.
+        {
+            var result = _productService.Add(product);
+            if (result.Success)// işlem sonucu true ise
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
